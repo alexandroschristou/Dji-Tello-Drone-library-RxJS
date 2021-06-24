33ba001fab7168b5df634c5600ea7f640127b1cc
alexandroschristou <49516590+alexandroschristou@users.noreply.github.com>
2021-06-24 14:51:35 +0200

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ tello.streamon();
 tello.start();
 
 tello.monitor(tello.get_yaw, 0, 50, tello.rotate_clockwise, tello.rotate_counter_clockwise, 20);
-tello.monitor(tello.get_height, 120, 150, tello.move_up, tello.move_down, 20);
+tello.monitor(tello.get_height, 120, 150, tello.move_up, tello.move_down, 30);
 
 function DoubleBackFlip(){
   tello.flip_back();
